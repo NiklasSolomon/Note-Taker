fcad15556cde5a91dd5618e371721133d8ce77bd
Niklas Solomon <niklassolomon@gmail.com>
2022-08-30 19:37:42 -0700

    Parsed data in server, added routes folder/file

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 // Add dependencies
 const express = require('express');
 const fs = require('fs');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// Parse the data
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+app.listen(PORT, () =>
+  console.log(`Listening for requests on port ${PORT}! 🏎️`)
+);
