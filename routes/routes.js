329f61e2d6124a2eeb6504d23585ea36952b96c9
Niklas Solomon <niklassolomon@gmail.com>
2022-08-31 18:51:58 -0700

    previous routes broke the styling, reworked

--- routes/routes.js
+++ routes/routes.js
@@ -2,36 +2,44 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = app => {
-    fs.readFile('db/db.json', 'utf-8', (err, data) => {
-        if (err) throw err;
-
-        const notes = JSON.parse(data);
-        
-        // Get and Post routes
-        app.get('/api/notes', function(req, res) {
-            res.json(notes);
-        });
-
-        app.post('/api/notes', function(req, res) {
-            let newNote = req.body;
-            notes.push(newNote);
-            updateDb();
-        });
-
-        // View the routes
-        app.get('/notes', function(req, res) {
-            res.sendFile(path.join(__dirname, './public/notes.html'));
-        });
-
-        app.get('*', function(req, res) {
-            res.sendFile(path.join(__dirname, './public/index.html'));
-        });
-
-        function updateDb() {
-            fs.writeFile('db/db.json', JSON.stringify(notes), err => {
-                if (err) throw err;
-                return true;
-            });
-        }
+    
+    // Get and Post routes
+    app.get('/api/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../db/db.json'));
     });
-}
+
+    app.post('/api/notes', (req, res) => {
+        let db = fs.readFileSync('db/db.json');
+        db = JSON.parse(db);
+        res.json(db);
+    
+        let newNote = {
+          title: req.body.title,
+          text: req.body.text,
+        };
+        
+        db.push(newNote);
+        fs.writeFileSync('db/db.json', JSON.stringify(db));
+        res.json(db);
+      });
+
+    // Delete route
+    app.delete('/api/notes/:id', (req, res) => {
+
+        let db = JSON.parse(fs.readFileSync('db/db.json'))
+
+        let deleteNotes = db.filter(item => item.id !== req.params.id);
+        
+        fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+            res.json(deleteNotes);
+    });
+  
+    // View the routes
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+};
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Add dependencies
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
+const routes = require('./routes/routes.js');
 
 const app = express();
 
@@ -9,7 +11,8 @@ const PORT = process.env.PORT || 3001;
 // Parse the data
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static('public'));
+app.use('/', routes);
 
 app.listen(PORT, () =>
   console.log(`Listening for requests on port ${PORT}!`)
