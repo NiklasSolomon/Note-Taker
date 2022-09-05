7a217efc6cf31fe6f9aab16f5d6b1f5f494a74dc
Niklas Solomon <niklassolomon@gmail.com>
2022-09-05 15:02:19 -0700

    changed app to router

--- routes/routes.js
+++ routes/routes.js
@@ -1,47 +1,48 @@
 const fs = require('fs');
 const path = require('path');
 const uuid = require('../helpers/uuid');
+const express = require('express');
+const router = express.Router();
 
-module.exports = (app) => {
+// Get and Post routes
+router.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../db/db.json'));
+});
+
+router.post('/api/notes', (req, res) => {
+    let db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db);
+    res.json(db);
+
+    let newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uuid()
+    };
     
-    // Get and Post routes
-    app.get('/api/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../db/db.json'));
-    });
+    db.push(newNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    res.json(db);
+  });
 
-    app.post('/api/notes', (req, res) => {
-        let db = fs.readFileSync('db/db.json');
-        db = JSON.parse(db);
-        res.json(db);
+// Delete route
+router.delete('/api/notes/:id', (req, res) => {
     
-        let newNote = {
-          title: req.body.title,
-          text: req.body.text,
-          id: uuid()
-        };
-        
-        db.push(newNote);
-        fs.writeFileSync('db/db.json', JSON.stringify(db));
-        res.json(db);
-      });
+    let db = JSON.parse(fs.readFileSync('db/db.json'))
 
-    // Delete route
-    app.delete('/api/notes/:id', (req, res) => {
-        
-        let db = JSON.parse(fs.readFileSync('db/db.json'))
-
-        let deleteNotes = db.filter(item => item.id !== req.params.id);
-        
-        fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
-            res.json(deleteNotes);
-    });
-  
-    // View the routes
-    app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
-    });
+    let deleteNotes = db.filter(item => item.id !== req.params.id);
     
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-};
+    fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+        res.json(deleteNotes);
+});
+
+// View the routes
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-require('./routes/routes')(app);
+app.use(routes)
+
+// require('./routes/routes')(app);
 
 app.listen(PORT, () =>
   console.log(`Listening for requests on port ${PORT}!`)
